50ff1ed4095b0434d60f087a2d62d111d5d12009
NdaaXD <98279739+NdaaXD@users.noreply.github.com>
2022-04-22 00:03:45 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 let fs = require('fs')
 
-global.owner = ['6289625556161'] // Put your number here
-global.mods = [] // Want some help?
+global.owner = ['6282145614361'] // Put your number here
+global.mods = ['6282145614361'] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
   // name: 'https://website'
@@ -28,11 +28,11 @@ global.APIKeys = { // APIKey Here
 
 // Sticker WM
 global.stiker_wait = 'Stiker sedang dibuat'
-global.packname = 'ZeusBotz MD'
-global.author = 'Zeus'
-global.email = 'wakabahiiro5@gmail.com'
+global.packname = ''
+global.author = ''
+global.email = 'Bundasut1960@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '© ZEUSBOTZ-MD'
+global.wm = '© NdaaBotz-MD'
 global.media = 'https://telegra.ph/file/f96eee5c2c75bd32c1f58.png'
 
 global.wait = '_*tunggu sedang di proses...*_'
